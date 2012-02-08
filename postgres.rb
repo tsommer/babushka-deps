5383ba6ad612c2f6bd037ccc21ff828186e1f2fa
Tom <tom@trikeapps.com>
2012-02-08 22:43:13 +1100

    Make it clear that we need the DB user.

--- postgres.rb
+++ postgres.rb
@@ -1,12 +1,12 @@
-dep "postgres-setup-db", :db_name, :username do
+dep "postgres-setup-db", :db_name, :db_username do
   met? {
-    result = sudo %Q{psql -c "SELECT rolname FROM pg_roles WHERE rolname = '#{username}'"}, :as => 'postgres'
+    result = sudo %Q{psql -c "SELECT rolname FROM pg_roles WHERE rolname = '#{db_username}'"}, :as => 'postgres'
     result =~ /1 row/
   }
 
   meet {
-    sudo "createuser -R -D -A -P #{username}", :as => 'postgres'
-    sudo "createdb -O #{username} #{db_name}", :as => 'postgres'
-    puts "Database '#{db_name}' created. User: '#{username}'"
+    sudo "createuser -R -D -A -P #{db_username}", :as => 'postgres'
+    sudo "createdb -O #{db_username} #{db_name}", :as => 'postgres'
+    puts "Database '#{db_name}' created. User: '#{db_username}'"
   }
 end
