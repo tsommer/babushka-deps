2e74a0cd56a340f4afdd5cb9f64f5ad94dc33e3a
Tom <tom@trikeapps.com>
2012-02-09 17:04:26 +1100

    First cut of custom up-to-date.repo dep.

--- git.rb
+++ git.rb
@@ -16,3 +16,23 @@ dep "replace-post-receive-hook", :path do
     end
   }
 end
+
+dep "up to date.repo", :git_ref_data, :env do
+  env.default!(ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'production')
+  requires [
+    'benhoskings:on correct branch.repo'.with(ref_info[:branch]),
+    'benhoskings:HEAD up to date.repo'.with(ref_info)
+    # 'app bundled'.with(:root => '.', :env => env),
+
+    # This and 'after deploy' below are separated so the deps in 'current dir'
+    # they refer to load from the new code checked out by 'HEAD up to date.repo'.
+    # Normally it would be fine because dep loading is lazy, but the "if Dep('...')"
+    # checks trigger a source load when called.
+    # 'on deploy'.with(ref_info[:old_id], ref_info[:new_id], ref_info[:branch], env),
+
+    # 'app flagged for restart.task',
+    # 'maintenance page down',
+    # 'after deploy'.with(ref_info[:old_id], ref_info[:new_id], ref_info[:branch], env)
+  ]
+
+end
