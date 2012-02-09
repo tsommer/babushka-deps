3d2497039ed3614ba52d852c8bbfd280b76a609f
Tom <tom@trikeapps.com>
2012-02-09 17:57:11 +1100

    Add dep to clone secrets repo.

--- git.rb
+++ git.rb
@@ -1,9 +1,10 @@
-dep "web repo", :path do
+dep "web repo with secrets", :path do
   requires "benhoskings:web repo".with(path),
-           "replace-post-receive-hook".with(path)
+           "replace-post-receive-hook".with(path),
+           "clone-secrets-repo"
 end
 
-dep "replace-post-receive-hook", :path do
+dep "replace-post-receive-hook", :path, :secrets_repo do
   met? {
     (path / ".git/hooks/post-receive").executable? &&
     Babushka::Renderable.new(path / ".git/hooks/post-receive").from?(dependency.load_path.parent / "git/deploy-repo-post-receive")
@@ -17,23 +18,12 @@ dep "replace-post-receive-hook", :path do
   }
 end
 
-dep "up to date.repo", :git_ref_data, :env do
-  env.default!(ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'production')
-  requires [
-    'benhoskings:on correct branch.repo'.with(ref_info[:branch]),
-    'benhoskings:HEAD up to date.repo'.with(ref_info),
-    'benhoskings:app bundled'.with(:root => '.', :env => env)
+dep "clone-secrets-repo", :secrets_repo do
+  met? {
+    met? { ("~" / "secrets" / '.git').dir? }
+  }
 
-    # This and 'after deploy' below are separated so the deps in 'current dir'
-    # they refer to load from the new code checked out by 'HEAD up to date.repo'.
-    # Normally it would be fine because dep loading is lazy, but the "if Dep('...')"
-    # checks trigger a source load when called.
-    # 'on deploy'.with(ref_info[:old_id], ref_info[:new_id], ref_info[:branch], env),
-
-    # 'app flagged for restart.task',
-    # 'maintenance page down',
-    # 'after deploy'.with(ref_info[:old_id], ref_info[:new_id], ref_info[:branch], env)
-  ]
-
+  meet {
+    shell "git clone #{secrets_repo} ~/secrets"
+  }
 end
-
