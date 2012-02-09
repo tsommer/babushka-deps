bf4978c8c7027b0ed8ab9ae63b8b1d2795c4dcd0
Tom <tom@trikeapps.com>
2012-02-09 19:58:46 +1100

    Ensure the configs are updated after deploy.

--- git.rb
+++ git.rb
@@ -25,3 +25,34 @@ dep "clone-secrets-repo", :secrets_repo do
     shell "git clone #{secrets_repo} ~/secrets"
   }
 end
+
+dep 'up to date.repo', :git_ref_data, :env do
+  env.default!(ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'production')
+  requires [
+    'benhoskings:on correct branch.repo'.with(ref_info[:branch]),
+    'benhoskings:HEAD up to date.repo'.with(ref_info),
+    'update-configs'.with(:root => "."),
+    'benhoskings:app bundled'.with(:root => '.', :env => env),
+
+    # This and 'after deploy' below are separated so the deps in 'current dir'
+    # they refer to load from the new code checked out by 'HEAD up to date.repo'.
+    # Normally it would be fine because dep loading is lazy, but the "if Dep('...')"
+    # checks trigger a source load when called.
+    'benhoskings:on deploy'.with(ref_info[:old_id], ref_info[:new_id], ref_info[:branch], env),
+
+    'benhoskings:app flagged for restart.task',
+    'benhoskings:maintenance page down',
+    'benhoskings:after deploy'.with(ref_info[:old_id], ref_info[:new_id], ref_info[:branch], env)
+  ]
+end
+
+dep "update-configs", :root do
+  met? { @run }
+
+  meet {
+    shell "cd ~/secrets && git pull"
+    shell "cd #{root}/config && for f in $(ls -d ~/secrets/config/*); do ln -fs $f; done"
+
+    @run = true
+  }
+end
