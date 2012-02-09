8d45bcc4d2b449558bb1b56990a60000f121efe3
Tom <tom@trikeapps.com>
2012-02-09 18:10:27 +1100

    Attempt to fix psych error.

--- git.rb
+++ git.rb
@@ -19,7 +19,7 @@ dep "replace-post-receive-hook", :path, :secrets_repo do
 end
 
 dep "clone-secrets-repo", :secrets_repo do
-  met? { ("~" / "secrets" / '.git').dir? }
+  met? { ('~' / 'secrets' / '.git').dir? }
 
   meet {
     shell "git clone #{secrets_repo} ~/secrets"
