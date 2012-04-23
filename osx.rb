14a4fb6587a112601e05b10a46ba170fa79d2bdb
Tom <tom@trikeapps.com>
2012-04-23 14:16:42 +1000

    Comment out osx commands until they're wrapped in deps.

--- osx.rb
+++ osx.rb
@@ -1,37 +1,37 @@
-# Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+## Expand save panel by default
+#defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
-# Expand print panel by default
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+## Expand print panel by default
+#defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 
-# Disable Resume system-wide
-defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+## Disable Resume system-wide
+#defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
 
-# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+## Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+#defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
+## Set a blazingly fast keyboard repeat rate
+#defaults write NSGlobalDomain KeyRepeat -int 0
 
-# Show all filename extensions in Finder
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+## Show all filename extensions in Finder
+#defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Show status bar in Finder
-defaults write com.apple.finder ShowStatusBar -bool true
+## Show status bar in Finder
+#defaults write com.apple.finder ShowStatusBar -bool true
 
-# Disable the warning when changing a file extension
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+## Disable the warning when changing a file extension
+#defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Automatically hide and show the Dock
-defaults write com.apple.dock autohide -bool true
+## Automatically hide and show the Dock
+#defaults write com.apple.dock autohide -bool true
 
-# Enable Safari’s debug menu
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+## Enable Safari’s debug menu
+#defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-# Disable the Ping sidebar in iTunes
-defaults write com.apple.iTunes disablePingSidebar -bool true
+## Disable the Ping sidebar in iTunes
+#defaults write com.apple.iTunes disablePingSidebar -bool true
 
-# Disable all the other Ping stuff in iTunes
-defaults write com.apple.iTunes disablePing -bool true
+## Disable all the other Ping stuff in iTunes
+#defaults write com.apple.iTunes disablePing -bool true
 
 
