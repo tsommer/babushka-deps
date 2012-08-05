fbdf288d1e13b766ac40b701e3e00c00593a131e
Tom <tom@trikeapps.com>
2012-08-06 08:03:51 +1000

    Add some more OSX settings.
    Thanks to
    https://github.com/mathiasbynens/dotfiles/blob/master/.osx?os-x-10.8

--- osx.rb
+++ osx.rb
@@ -1,10 +1,10 @@
 dep "osx-settings.task" do
 
   run {
-    log_shell "Expand save panel by default",
+    log_shell "Expand expanded save panel by default",
       "defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true"
 
-    log_shell "Expand print panel by default",
+    log_shell "Expand expanded print panel by default",
       "defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true"
 
     log_shell "Disable Resume system-wide",
@@ -13,20 +13,37 @@ dep "osx-settings.task" do
     log_shell "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)",
       "defaults write NSGlobalDomain AppleKeyboardUIMode -int 3"
 
-    log_shell "Set a blazingly fast keyboard repeat rate",
-      "defaults write NSGlobalDomain KeyRepeat -int 0"
+    log_shell "Disable press-and-hold in favour of key repeat",
+      "defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false"
 
-    log_shell "Set a short initial keyboard delay time",
-      "defaults write NSGlobalDomain InitialKeyRepeat -int 15"
+    # Set through KeyRemap4MacBook: 23 ms
+    #log_shell "Set a blazingly fast keyboard repeat rate",
+    #  "defaults write NSGlobalDomain KeyRepeat -int 0"
+
+    # Set through KeyRemap4MacBook: 400 ms
+    #log_shell "Set a short initial keyboard delay time",
+    #  "defaults write NSGlobalDomain InitialKeyRepeat -int 15"
 
     log_shell "Show all filename extensions in Finder",
       "defaults write NSGlobalDomain AppleShowAllExtensions -bool true"
+
+    log_shell "Show hidden files in Finder",
+      "defaults write com.apple.Finder AppleShowAllFiles -bool true"
 
     log_shell "Show status bar in Finder",
       "defaults write com.apple.finder ShowStatusBar -bool true"
 
     log_shell "Disable the warning when changing a file extension",
       "defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false"
+
+    log_shell "Don't create .DS_Store files on network volumes",
+      "defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true"
+
+    log_shell "Show ~/Library folder",
+      "chflags nohidden ~/Library"
+
+    log_shell "Allow text selection in QuickLook",
+      "defaults write com.apple.finder QLEnableTextSelection -bool true"
 
     log_shell "Automatically hide and show the Dock",
       "defaults write com.apple.dock autohide -bool true"
@@ -46,11 +63,20 @@ dep "osx-settings.task" do
     log_shell "Disable local Time Machine snapshots",
       "sudo tmutil disablelocal"
 
+    log_shell "Prevent TimeMachine from asking for new backup disks",
+      "defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true"
+
     log_shell "Disable hibernate",
       "sudo pmset -a hibernatemode 0"
 
     log_shell "Turn off sudden motion sensor",
       "sudo pmset -a sms 0"
+
+    log_shell "Disable menu bar transaparency",
+      "defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false"
+
+    log_shell "Enable debug menu in DiskUtility",
+      "defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true"
   }
 end
 
